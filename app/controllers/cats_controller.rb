585eb4530ec6fc0dbe08e3d4367a0ec3f24cf478
yianlo <yianlo712@gmail.com>
2016-01-26 15:23:16 -0800

    adds edit view and edit method to controller

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -16,6 +16,30 @@ class CatsController < ApplicationController
   def new
     render :new
   end
+
+  def create
+    @new_cat = Cat.new(cat_params)
+    if @new_cat.save
+      redirect_to cats_url
+    else
+      render json: @new_cat.errors.full_messages
+    end
+  end
+
+  def edit
+    @cat = Cat.find_by(id: params[:id])
+    
+    render :edit
+  end
+
+  def update
+    @cat = Cat.find_by(id: params[:id])
+    @cat.update(cat_params)
+
+    redirect_to cats_url
+  end
+
+
   private
 
   def cat_params
